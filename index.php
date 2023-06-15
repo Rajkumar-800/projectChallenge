11dff0429e8bf90932a2a748c42c688fd6987570
Raj <raj.ecomitsol@gmail.com>
2023-06-15 10:47:29 +0530

    three files new add

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>DigiStrike</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <!-- link css  -->
+    <link rel="stylesheet" href="style.css">
+    <!-- font awesome cdn  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+<body> 
+
+    <section class="para1"> 
+ <div class="head mx-5 my-5 " > <h1>SEO Packages That Drive More Traffic, Leads, and Sales From <span class="text-center"><br>Search</span> </h1></div>
+ <div class="container fluid " > 
+   <div class="row "> 
+   <div class="col-lg-6 "> 
+        <p class="paragraph">SEO packages are concierage search engine optimization services, in which a specialist business dedicates itself to improving search engine result. SEO packages will concentratre on the 250+ ranking elements that Google takes into accont when chooosing a page ranking in search results. </span ><span > <br><br> SEO monthly packages consist of a group of search engine optimization services that serve the organic internet marketing demands of a business. These SEO packages are often offered by digital marketing agencies and they aid businesses by increasing its visibility, Search rankings, and website traffic.  <span><br> At Rankings, we help you get startes with right, affordable SEO packages. Each type of search engine optimization packages serves its purpose . Our Local SEO packages are ideal for businesses that wish to attract customers locally or in a specific region . Opting for the National SEO packages is the best if you are looking for enterprise or B2B leads. It's also suitable if your final goal is to get more overseas business leads attractions. </span></p>
+        </div>
+      
+        <div class="col-lg-6"> 
+            <img src="./images/seo-package-img.webp" alt="" width="450px">
+
+        </div>
+        <div class="paragraph2"> When it  comes to procuct-selling bussinesses , our E-commerece SEO packages will help your products rank faster and improve the growth rate of your E-commerce platform or website whether you sell a few procucts or offer a full inventory , our E-commerece SEO packages can ger you going . </span> <span><br>Thus we have search engine optimization packages for businesses of all types . Want one for Your business? Then , start today with our flexible SEO packages India.</div>
+
+   </div>
+ </div>
+ </section> 
+ <!-- local seo result here  -->
+ <section class="ls-result empowering-sec"> 
+  <div class="container"> 
+    <div class="row"> 
+ <div class="col-md-12"> 
+  <div class="what-make-txt-ui text-center"> 
+    <h2>Check Out Our Deliverable SEO Results!</h2>
+  </div>
+ </div>
+ </div>
+ <div class="row"> 
+  <div class="col-md-3">
+    <div class="empowering-card"> 
+      <div class="icon"> 
+        <img src="" alt="">
+      </div>
+      <div class="empowering-content"> 
+        <p>We’ve generated REVENUE Annually For Our CLIENTs</p>
+        <h3>$3,021,182,299</h3>
+      </div>
+    </div>
+  </div>
+
+ </div>
+ </div>
+ </section>
+ 
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
